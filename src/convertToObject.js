b8165b7e56b8149817e813c223b2a30077c0d090
Rustam Herheman <gergemannn@gmail.com>
2025-03-18 20:06:12 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,21 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
-
-  sourceString
-    .split(';')
-    .map((s) => s.trim())
-    .filter(Boolean)
-    .forEach((declaration) => {
+  return sourceString
+    .split(';') // Розбиваємо рядок на декларації
+    .map((s) => s.trim()) // Очищаємо кожну декларацію від зайвих пробілів
+    .filter(Boolean) // Фільтруємо порожні рядки
+    .reduce((result, declaration) => {
       const [property, value] = declaration.split(':').map((s) => s.trim());
 
       if (property && value) {
-        result[property] = value;
+        result[property] = value; // Додаємо пару property-value в об'єкт
       }
-    });
 
-  return result;
+      return result; // Повертаємо оновлений об'єкт
+    }, {});
 }
 
 module.exports = convertToObject;
